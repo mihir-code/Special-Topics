fd1cfa5cc6a994879a8229754f676b0fe522bf4e
Mihir <85762703+mihir-code@users.noreply.github.com>
2023-09-17 23:14:30 +0000

    added the Percolation and PercolationStats data

---
+++ Percolation.java
@@ -0,0 +1,25 @@
+public class Percolation{
+    private int opensites;
+    
+
+    public Percolation(int n){
+
+
+    }
+    public void open (int row, int col){
+
+    }
+    public boolean isOpen(int row, int col){
+
+    }
+    public boolean isFull(int row, int col){
+        
+    }
+    public int numberOfOpenSites(){
+
+    }
+    public boolean percolates(){
+
+    }
+
+}
---
+++ PercolationStats.java
@@ -0,0 +1,18 @@
+public class PercolationStats {
+
+    public PercolationStats(int n, int trials){
+
+    }
+    public double mean(){
+
+    }
+    public double stddev(){
+        
+    }
+    public double confidenceLo(){
+
+    }
+    public double confidenceHi(){
+        
+    }
+}
