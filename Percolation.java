99c304a12514f742d87f6906913a3ba41fe4219b
Mihir <85762703+mihir-code@users.noreply.github.com>
2023-09-17 23:22:47 +0000

    added the illegeal argumentseception  in 4 methods

--- Percolation.java
+++ Percolation.java
@@ -1,18 +1,33 @@
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 public class Percolation{
     private int opensites;
+    private int n;
+    private boolean [] sites;
     
 
     public Percolation(int n){
+        if (n <= 0)
+        throw new IllegalArgumentException();
+        sites = new boolean [n*n];
+
 
 
     }
     public void open (int row, int col){
+        if (row > n || row < 1 || col > n || col < 1)
+        throw new IllegealArgumentException();
 
     }
     public boolean isOpen(int row, int col){
+        if (row > n || row < 1 || col > n || col < 1)
+        throw new IllegealArgumentException();
 
     }
     public boolean isFull(int row, int col){
+        if (row > n || row < 1 || col > n || col < 1)
+        throw new IllegealArgumentException();
+        
         
     }
     public int numberOfOpenSites(){
