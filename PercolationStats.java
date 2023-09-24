c27db3c29ddd2e9c885765eb08f9329576285306
Mihir <85762703+mihir-code@users.noreply.github.com>
2023-09-24 22:40:04 +0000

    added some code to the constructor in stats

--- PercolationStats.java
+++ PercolationStats.java
@@ -2,15 +2,30 @@ import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
-    private grid[][];
-    private int value;
+    private double[] grid;
+    private int count;
 
     public PercolationStats(int n, int trials){
         if (n <=0 || trials <=0)
         throw new IllegalAccessError();
+        count = trials;
+        grid = new double[count];
+        for (int numexperiments = 0; numexperiments < count; numexperiments++){
+            Percolation x = new Percolation(n);
+            int opensites = 0;
+            while (!x.percolates()) {
+                int row = StdRandom.uniformInt(1, n+1);
+                int col = StdRandom.uniformInt(1, n+1)
+                if (!x.isOpen(row, col)){
+                    x.open(row,col);
+                    opensites++;
+                }
+            }
+        }
 
     }
     public double mean(){
+        
 
     }
     public double stddev(){
