20222b49dbc907ffbf5f4af741ff9302eed34db3
Mihir <85762703+mihir-code@users.noreply.github.com>
2023-09-29 18:41:12 +0000

    adding brackets to the confidence interval

--- PercolationStats.java
+++ PercolationStats.java
@@ -49,8 +49,8 @@ public class PercolationStats {
         int trials = Integer.parseInt(args[1]);
         PercolationStats stats = new PercolationStats(n, trials);
 
-        System.out.println("mean is                 = " + stats.mean());
-        System.out.println("stddev is               = " + stats.stddev());
-        System.out.println("95% confidence interval = " + stats.confidenceLo() + ", " + stats.confidenceHi());
+        System.out.println("mean is = " + stats.mean());
+        System.out.println("stddev is = " + stats.stddev());
+        System.out.println("95% confidence interval = " + "[ " + stats.confidenceLo() + ", " + stats.confidenceHi() + "]");
     }
 }
