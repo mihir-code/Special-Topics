f389213bd7bfb3591ae255e1e98dd5172df99972
Mihir <85762703+mihir-code@users.noreply.github.com>
2023-10-06 19:03:37 +0000

    moved percol and created new files for new assignment

---
+++ Deque.java
@@ -0,0 +1,35 @@
+public class Deque<Item> implements Iterable<Item> {
+    int size;
+    
+
+    public Deque(){
+        
+    }
+    public boolean isEmpty(){
+
+    }
+    public int size(){
+
+
+    }
+    public void addFirst(Item item){
+
+    }
+    public void addLast(Item item){
+
+    }
+    public Item removeFirst(){
+
+    }
+    public Item removeLast(){
+
+    }
+    public Iterator<Item> iterator(){
+
+    }
+    public static void main(String args[]){
+
+    }
+
+
+}
