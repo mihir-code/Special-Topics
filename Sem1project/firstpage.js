565d379df47f9fe697c8c5109c656b08e4a1b477
Mihir <85762703+mihir-code@users.noreply.github.com>
2023-10-27 04:56:39 +0000

    experimenting with a new idea

--- Sem1project/firstpage.js
+++ Sem1project/firstpage.js
@@ -4,3 +4,17 @@ var dal;
 var curry;
 var rcup;
 var water;
+function auth(){
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+
+    if (email === "mihir.motukuri@Gmail.com" && password === "1234"){
+        window.location.replace("ricepage.html");
+
+    }
+    else {
+        alert("Invalid. Please try again");
+        return;
+    }
+
+}
