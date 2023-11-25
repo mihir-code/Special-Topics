c01c68011df9836b984e3cedfcf04eba17fde940
Mihir <85762703+mihir-code@users.noreply.github.com>
2023-11-25 17:41:24 +0000

    making it possible to store the login

--- Sem1project/login.php
+++ Sem1project/login.php
@@ -1,4 +1,17 @@
 <?php
 require_once "conf.php";
 require_once "session.php";
+$error = '';
 
+if ($_SERVER["Request_Method"] == "post" && isset($_POST['submit'])){
+    $email = trim($_POST['email']);
+    $password = trim($_POST['password']);
+
+    if(empty($email)){
+        $error .= '<p class="error">Enter the email.</p>';
+    }
+    if(empty($password)){
+        $error .='<p class="error">Enter the password.</p>';
+    }
+    
+}
