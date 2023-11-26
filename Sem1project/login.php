483664938c7826e55cd11a1eaa905fd454249c0b
Mihir <85762703+mihir-code@users.noreply.github.com>
2023-11-26 01:12:26 +0000

    finished debugging login page

--- Sem1project/login.php
+++ Sem1project/login.php
@@ -4,37 +4,38 @@ require_once "session.php";
 $error = '';
 
 if ($_SERVER["Request_Method"] == "post" && isset($_POST['submit'])){
-}
-    $email = trim($_POST['email']);
-    $password = trim($_POST['password']);
-
+    $email = trim($_POST["Email"]);
+    $password = trim($_POST["Password"]);
     if(empty($email)){
-        $error .= '<p class="error">Enter the email.</p>';
+        $error .= '<p class="error"> Please enter in your email.</p>';
     }
     if(empty($password)){
-        $error .='<p class="error">Enter the password.</p>';
+        $error .= '<p class="error"> Please enter in your password.</p>';
     }
     if(empty($error)){
-        if($query = $db->prepare("SELECT * FROM users WHERE email = ?")){
-            $query->bind_param("s", $email);
+        if($query = $db->prepare("SELECT * FROM users WHERE email =?")){
+            $query->bind_param("s",$email);
             $query->execute();
-            $row =$query->get_result();
+            $row = $query->get_result();
             if($row){
-                if(password_verify($password, $row->password)){ 
-                    $_SESSION["userid"] = $row->id;
-                    $_SESSION["user"] = $row;
+                if(password_verify($password,$row["password"])){
+                    $_SESSION["userid"] = $row["id"];
+                    $_SESSION["user"] =$row;
                     header("location: welcome.php");
                     exit;
+                }
+                else{
+                    $error .= '<p class="error"> Wrong password. Try again.</p>';
+                }
             }
             else{
-                $error .= '<p class="error">Password is not valid.</p>';
+                $error .= '<p class="error">Unknown email address. Try again.</p>';
             }
-
         }
-        else{
-            $error .= '<p class="error">Email Address is not tied to any user.</p>';
-        }
-        
+        $query->close();
     }
-    quer
+    mysqli_close($db);
 }
+
+    // find a way to tie in previous welcome page.
+
