5806d511140fe41b2dd631fd8828f0639f0c6e6f
Mihir <85762703+mihir-code@users.noreply.github.com>
2023-11-26 01:00:25 +0000

    debugging

--- Sem1project/login.php
+++ Sem1project/login.php
@@ -4,6 +4,7 @@ require_once "session.php";
 $error = '';
 
 if ($_SERVER["Request_Method"] == "post" && isset($_POST['submit'])){
+}
     $email = trim($_POST['email']);
     $password = trim($_POST['password']);
 
@@ -14,6 +15,26 @@ if ($_SERVER["Request_Method"] == "post" && isset($_POST['submit'])){
         $error .='<p class="error">Enter the password.</p>';
     }
     if(empty($error)){
-        if($query = $db->prepare("SELECT * FROM users WHERE email = ?"))
+        if($query = $db->prepare("SELECT * FROM users WHERE email = ?")){
+            $query->bind_param("s", $email);
+            $query->execute();
+            $row =$query->get_result();
+            if($row){
+                if(password_verify($password, $row->password)){ 
+                    $_SESSION["userid"] = $row->id;
+                    $_SESSION["user"] = $row;
+                    header("location: welcome.php");
+                    exit;
+            }
+            else{
+                $error .= '<p class="error">Password is not valid.</p>';
+            }
+
+        }
+        else{
+            $error .= '<p class="error">Email Address is not tied to any user.</p>';
+        }
+        
     }
+    quer
 }
