7ec5d64ca88e6bb93d438361578fa5bcf983f6f5
Mihir <85762703+mihir-code@users.noreply.github.com>
2023-12-26 05:35:28 +0000

    making pages

---
+++ KdTree.java
@@ -0,0 +1,26 @@
+public class KdTree{
+    public KdTree(){
+
+    }
+    public boolean isEmpty(){
+
+    }
+    public int size(){
+
+    }
+    public void insert(Point2D p){
+
+    }
+    public boolean contains(Point2D p){
+
+    }
+    public void draw(){
+
+    }
+    public Iterable<Point2d> range(RectHV rect){
+
+    }
+    public Point2D nearest(Point2D p){
+        
+    }
+}
---
+++ PointSET.java
@@ -0,0 +1,100 @@
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+/*
+public class Point2D implements Comparable <Point2D>{
+    public Point2D(double x, double y){
+
+    }
+    public double x(){ // x-coordinate
+
+    }
+    public double y(){ // y-coordinate
+
+    } 
+    public double distanceTo(Point2D that){
+
+    }
+    public distanceSquaredTo(Point2D that){ // slope?
+
+    }
+    public int compareTo(Point2D that){
+
+    }
+    public boolean equals(Object that){
+
+    }
+    public void draw(){
+
+    }
+    public String toString(){
+
+    }
+
+}
+*/
+/*
+public class RectHV{
+    public RectHV(double xmin, double ymin, double xmax, double ymax){ //make triangle
+
+    }
+    public double xmin(){
+
+    }
+    public double ymin(){
+
+    }
+    public double xmax(){
+
+    }
+    public double ymax(){
+
+    }
+    public boolean contains(Point2D p){
+
+    }
+    public boolean intersects(RectHv that){
+
+    }
+    public double distanceTo(Point2D p){
+
+    }
+    public double distanceSquaredTo(Point2D p){
+
+    }
+    public boolean equals(Object that){
+
+    }
+    public void draw(){
+        
+    }
+    public String toString(){
+
+    }
+}
+*/
+public class PointSET{
+    public PointSET(){
+
+    }
+    public boolean isEmpty(){ //is the set empty?
+
+    }
+    public int size(){
+
+    }
+    public void insert(Point2D p){
+
+    }
+    public boolean contains(Point2D p){
+
+    }
+    public void draw(){
+
+    }
+    public Iterable<Point2D> range(RectHV rect){
+
+    }
+    public Point2D nearest(Point2D p){
+        
+    }
+}
