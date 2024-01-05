b1cbab7a9427af4d5d2bb427a08ca193d4fd6762
Mihir <85762703+mihir-code@users.noreply.github.com>
2024-01-05 19:50:33 +0000

    making some progress on insert

--- kd-trees submission/KdTree.java
+++ kd-trees submission/KdTree.java
@@ -5,12 +5,12 @@ import edu.princeton.cs.algs4.SET;
 
 public class KdTree{
     private int size = 0;
-    private Node root = null;
+    private Node root = null; // very important.
     private static class Node{
         private Point2D p;
         private RectHV rect;
-        private Node lb;
-        private Node rt;
+        private Node lb; // left bottom
+        private Node rt; // right top
     }
     public Node(Point2D p, RectHV rect, Node lb, Node rt){
         p = this.p;
@@ -28,12 +28,43 @@ public class KdTree{
 
     }
     public void insert(Point2D p){
+        return insertt(Node p2, Point2d p);
+
+        
+
+    }
+    private void insert(Node p2, Point2D p){
+        if(root == null){
+            return new Node(p, null, null, null);
+        }
+        size++;
+        int x = p2.compareTo(p);
+        else if(x < 0){
+            p = p.left;
+        else if (x > 0){
+            p = p.right;
+        }
+        else{
+            return p.value;
+        }
+        return null;
+        }
 
     }
     public boolean contains(Point2D p){
+        if(!insert(Point2d p){
+            return false;
+        })
+        else{
+            return true;
+        }
+
 
     }
     public void draw(){
+        StdDraw(p);
+        StdDraw(rect);
+
 
     }
     public Iterable<Point2d> range(RectHV rect){
