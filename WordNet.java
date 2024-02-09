262cc0ada1901446a9d394f0d697c56af62fd823
Mihir <85762703+mihir-code@users.noreply.github.com>
2024-02-09 19:23:56 +0000

    created the pages

---
+++ WordNet.java
@@ -0,0 +1,18 @@
+public class WordNet{
+
+    public WordNet(String synsets, String hypernyms){
+
+    }
+    public Iterable<String> nouns(){
+
+    }
+    public distance(String nounA, String nounB){
+        
+    }
+    public String sap(String nounA, String nounB){
+        
+    }
+    public static void main(String[] args){
+        
+    }
+}
