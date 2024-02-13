3df9f2e9a3c60059057c44707a6123c5a363fa7e
Mihir <85762703+mihir-code@users.noreply.github.com>
2024-02-13 19:43:37 +0000

    finished with the input

--- WordNet.java
+++ WordNet.java
@@ -2,22 +2,26 @@ import edu.princeton.cs.algs4.BFS;
 import edu.princeton.cs.algs4.in;
 import edu.prinecton.cs.algs4.SAP;
 import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.Hashmap;
 
 
 public class WordNet{
 
-    private final Hashmap<String, Bag<Integer>> tracker;
-    private final Hashmap<Integer, String> store;
+    private final Hashmap<Integer, String> idtosynsets;
+    private final Hashmap<String, Bag<Integer>> nountoid;
     private final SAP sap;
     private final Digraph g;
 
     public WordNet(String synsets, String hypernyms){
 
-        store = new Hashmap<Integer, String>(); // stores synsets
-        tracker = new Hashmap<String, Bag<Integer>>(); // stores nouns and related ids
+        idtosynsets = new Hashmap<Integer, String>(); // stores synsets
+        nountoid = new Hashmap<String, Bag<Integer>>(); // stores nouns and related ids
 
     }
-    private int Syn(String synsets){ // for reading the synsets
+    private int Syn(String synsets){ // for reading the 
+        if (synsets == null){
+            throw new IllegalArgumentException();
+        }
         In in = new In(synsets);
         int count = 0;
         while (in.HasNextLine()){
@@ -25,17 +29,17 @@ public class WordNet{
             String line = in.readline();
             String[] parts = line.split(","); // split by comma for parts
             int id = Integer.parseint(parts[0]);
-            store.put(id, parts[1]);
+            idtosynsets.put(id, parts[1]);
             String[] noun = parts[1].split(" "); //split by space for nouns
             for (String n: nouns){
-                if(store.get(n) != null){
-                    Bag<Integer> syn = store.get(n);
+                if(idtosynsets.get(n) != null){
+                    Bag<Integer> syn = idtosynsets.get(n);
                     syn.add(n);
                 }
                 else{
-                    Bag<Integer> syn = new Bag<Integer>();
+                    Bag<Integer> syn = new Bag<Integer>(); // if it's null, we just proceed
                     syn.add(id);
-                    store.put(n,syn);
+                    idtosynsets.put(n,syn);
                 }
             }
             count++;
@@ -44,12 +48,18 @@ public class WordNet{
     }
     
     private void Hyp(String hypernyms){ // for reading the hypnyms 
+        if (hypernyms == null){
+            throw new IllegalArgumentException();
+        }
         In in = new in(hypernyms);
         while (in.HasNextLine){
-            int line = in.readInt();
-            int[] parts = line.split();
+            String line = in.readString();
+            String[] parts = line.split(",");
             int id = Integer.parseInt(parts[0]);
-            
+            for(int i = 1; i < parts.length; i++){
+                int p = Integer.parseInt(parts[i]);
+                g.addEdge(id, p);
+            }
         }
 
     }
