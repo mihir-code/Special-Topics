e1797abd17fd47933ed1a7a4e235594d7a0ee343
Mihir <85762703+mihir-code@users.noreply.github.com>
2024-02-14 19:22:42 +0000

    making sure all the edge cases are covered

--- SAP.java
+++ SAP.java
@@ -19,14 +19,18 @@ public class SAP{
         if (w = null){
             throw new IllegalArgumentException();
         }
+        range(v,w);
         int length = Integer.MAX_VALUE; // need a big number
-        BreadthFirstDirectedPaths pathv = new BreadthFirstDirectedPaths();
-        BreadthFirstDirectedPaths pathw = new BreadthFirstDirectedPaths();
+        BreadthFirstDirectedPaths pathv = new BreadthFirstDirectedPaths(G, v);
+        BreadthFirstDirectedPaths pathw = new BreadthFirstDirectedPaths(G, w);
         for (int i = 0; i < g.V(); i++){
             if (pathv.HasPathTo(i) && pathw.HasPathTo(i)){
                 int vw = pathv.distTo(i) + pathw.distTo(i);
                 if (vw < length){
                     length = vw;
+                }
+                if (vw == length){
+                    return -1;
                 }
             }
             else{
@@ -40,30 +44,41 @@ public class SAP{
         if (w = null){
             throw new IllegalArgumentException();
         }
-        int length = Integer.MAX_VALUE; // need a big number
-        BreadthFirstDirectedPaths pathv = new BreadthFirstDirectedPaths();
-        BreadthFirstDirectedPaths pathw = new BreadthFirstDirectedPaths();
+        range(v,w);
+        int ancestor = Integer.MAX_VALUE; // need a big number
+        BreadthFirstDirectedPaths pathv = new BreadthFirstDirectedPaths(G, v);
+        BreadthFirstDirectedPaths pathw = new BreadthFirstDirectedPaths(G, w);
         for (int i = 0; i < g.V(); i++){
             if (pathv.HasPathTo(i) && pathw.HasPathTo(i)){
                 int vw = pathv.distTo(i) + pathw.distTo(i);
-                if (vw < length){
-                    length = vw;
+                if (vw < ancestor){
+                    ancestor = vw;
+                if (vw == ancestor){
+                    ancestor = -1;
+                }    
                 }
             }
             else{
                 return -1;
             }
         }
-        return length;
+        return ancestor;
     }
 
     public int length(Iterable<Integer> v, Iterable<Integer> w){
+
 
     }
     public int ancestor(Iterable<Integer> v, Iterable<Integer> w){
 
     }
-
+    private void range(int r){
+        int w = G.r();
+        if(r < 0 || o >= w){
+            throw new IllegalArgumentException();
+        }
+      
+    }
     public static void main(String[] args) {
         In in = new In(args[0]);
         Digraph G = new Digraph(in);
