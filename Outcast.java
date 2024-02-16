2349797c2665b339849d82ad3326ad0ea813148f
Mihir <85762703+mihir-code@users.noreply.github.com>
2024-02-16 19:47:23 +0000

    working on outcast

--- Outcast.java
+++ Outcast.java
@@ -2,11 +2,20 @@ import edu.princeton.cs.algs4.WordNet;
 
 
 public class Outcast{
-    private final out WordNet;
+    private final WordNet wordnet;
     public Outcast(WordNet wordnet){
+        this.wordnet = wordnet;
 
     }
     public String outcast(String[] nouns){
+        String out = noun[0];
+        int sum = Integer.MAX_VALUE;
+        for(int i = 1; i < nouns.length; i++){
+            s += wordnet.distance(noun[0],noun[i]);
+            if (sum < s){
+                return sum;
+            }
+        }
 
     }
     public static void main (String[] args){
--- WordNet.java
+++ WordNet.java
@@ -58,7 +58,6 @@ public class WordNet{
                     idtosynsets.put(n,syn);
                 }
             }
-            count++;
         }
         return count;
     }
