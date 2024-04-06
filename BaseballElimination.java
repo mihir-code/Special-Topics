2790ff4a016d484482114772411a5b48b7724280
Mihir <85762703+mihir-code@users.noreply.github.com>
2024-04-06 22:15:07 +0000

    reorganizing

---
+++ BaseballElimination.java
@@ -0,0 +1,39 @@
+public class BaseballElimination{
+
+
+public BaseballElimination(String filename){
+
+}
+
+public int numberofTeams(){
+
+}
+
+public Iterable<String> teams(){
+    
+}
+
+public int wins(String team){
+
+}
+
+public int losses(String team){
+
+}
+
+public int remaining(String team){
+
+}
+
+public int against(String team1, String team2){
+
+}
+
+public boolean isEliminated(String team){
+
+}
+
+public Iterable<String> certificateOfElimination(String Team){
+
+}
+}
