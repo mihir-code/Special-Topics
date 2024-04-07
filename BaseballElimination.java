e5153d9e94382857f1a110b13ed7ff89fb80683b
Mihir <85762703+mihir-code@users.noreply.github.com>
2024-04-07 19:40:55 +0000

    thinking about how to go about the problem

--- BaseballElimination.java
+++ BaseballElimination.java
@@ -1,16 +1,33 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.FordFulkerson;
+import edu.princeton.cs.algs4.FlowNetwork;
+import edu.princeton.cs.algs4.FlowEdge;
+
+import java.util.ArrayList;
+
+
+
+
 public class BaseballElimination{
+    private int numbofteams;
+    private ArrayList<String> teamnames;
 
 
 public BaseballElimination(String filename){
+    In in = new In(filename);
+    amountofteams
+    numbofteams = in.readInt();
 
 }
 
 public int numberofTeams(){
+    return numbofteams;
 
 }
 
 public Iterable<String> teams(){
-    
+    return teamnames;
 }
 
 public int wins(String team){
