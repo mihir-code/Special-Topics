46a9fe841708d5bea67f97c9e3bc49056dd2c060
Mihir <85762703+mihir-code@users.noreply.github.com>
2024-04-08 19:23:10 +0000

    after mapping out on paper, believe that I have an implementation method

--- BaseballElimination.java
+++ BaseballElimination.java
@@ -5,20 +5,31 @@ import edu.princeton.cs.algs4.FlowNetwork;
 import edu.princeton.cs.algs4.FlowEdge;
 
 import java.util.ArrayList;
-
+import java.util.HashMap;
 
 
 
 public class BaseballElimination{
+    private int[] victories;
+    private int[] gamesremaning;
+    private int[] losses;
+    private int[][] opponents;
     private int numbofteams;
-    private ArrayList<String> teamnames;
+    private ArrayList<String> actteams;
 
 
 public BaseballElimination(String filename){
-    In in = new In(filename);
-    amountofteams
-    numbofteams = in.readInt();
+    In input = new In(filename);
+    for (int i = 0; i < numbofteams; i++){
+        actteams.add(input.readString());
+        gamesremaning[i]= input.readInt();
+        victories[i]= input.readInt();
+        losses[i]= input.readInt();
+        for (int opp = 0; i < numbofteams; i++){
+            opponents[i][j] = input.readInt();
+        }
 
+    }
 }
 
 public int numberofTeams(){
@@ -27,23 +38,28 @@ public int numberofTeams(){
 }
 
 public Iterable<String> teams(){
-    return teamnames;
+    return actteams;
 }
 
 public int wins(String team){
-
+    int t = getTeam(team);
+    return victories[t];
 }
 
 public int losses(String team){
-
+    int t = getTeam(team);
+    return losses[t];
 }
 
 public int remaining(String team){
-
+    int t = getTeam(team);
+    return gamesremaning[t];
 }
 
 public int against(String team1, String team2){
-
+    int t1 = getTeam(team1);
+    int t2 = getTeam(team2);
+    return opponents[t1][t2];
 }
 
 public boolean isEliminated(String team){
@@ -52,5 +68,19 @@ public boolean isEliminated(String team){
 
 public Iterable<String> certificateOfElimination(String Team){
 
+}
+
+private getTeam(String team){
+    if (team == null){
+        throw new IllegalArgumentException();
+        for (int t = 0; t < numbofteams; t++){
+            if (t.get(teams).equals(team)){
+                return t;
+            }
+            else{
+                throw new IllegalArgumentException();
+            }
+        }
+    }
 }
 }
